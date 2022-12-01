4e8ed2920a72d3a70a8f066ead57f1766b456c75
Gabriel Bénédict <gbndict@gmail.com>
2022-12-01 13:49:48 +0100

    sparse matrix to torch tensor and print

--- recbole/model/general_recommender/recfusion.py
+++ recbole/model/general_recommender/recfusion.py
@@ -14,6 +14,108 @@ from recbole.model.abstract_recommender import GeneralRecommender
 
 
 
+########################
+
+
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+import torch
+from sklearn.datasets import load_digits
+from sklearn import datasets
+from torch.utils.data import Dataset, DataLoader
+import torch.nn as nn
+import torch.nn.functional as F
+import ranking_metrics as metric
+
+import pdb 
+
+# Auxiliary functions, classes such as distributions
+
+
+PI = torch.from_numpy(np.asarray(np.pi))
+EPS = 1.e-7
+
+
+def log_categorical(x, p, num_classes=256, reduction=None, dim=None):
+    x_one_hot = F.one_hot(x.long(), num_classes=num_classes)
+    log_p = x_one_hot * torch.log(torch.clamp(p, EPS, 1. - EPS))
+    if reduction == 'avg':
+        return torch.mean(log_p, dim)
+    elif reduction == 'sum':
+        return torch.sum(log_p, dim)
+    else:
+        return log_p
+
+
+def log_bernoulli(x, p, reduction=None, dim=None):
+    pp = torch.clamp(p, EPS, 1. - EPS)
+    log_p = x * torch.log(pp) + (1. - x) * torch.log(1. - pp)
+    if reduction == 'avg':
+        return torch.mean(log_p, dim)
+    elif reduction == 'sum':
+        return torch.sum(log_p, dim)
+    else:
+        return log_p
+
+
+def log_normal_diag(x, mu, log_var, reduction=None, dim=None):
+    log_p = -0.5 * torch.log(2. * PI) - 0.5 * log_var - \
+        0.5 * torch.exp(-log_var) * (x - mu)**2.
+    if reduction == 'avg':
+        return torch.mean(log_p, dim)
+    elif reduction == 'sum':
+        return torch.sum(log_p, dim)
+    else:
+        return log_p
+
+
+def log_standard_normal(x, reduction=None, dim=None):
+    log_p = -0.5 * torch.log(2. * PI) - 0.5 * x**2.
+    if reduction == 'avg':
+        return torch.mean(log_p, dim)
+    elif reduction == 'sum':
+        return torch.sum(log_p, dim)
+    else:
+        return log_p
+
+
+def log_min_exp(a, b, epsilon=1e-8):
+
+    y = a + torch.log(1 - torch.exp(b - a) + epsilon)
+
+    return y
+
+
+def log_integer_probability(x, mean, logscale):
+    scale = torch.exp(logscale)
+
+    logp = log_min_exp(
+        F.logsigmoid((x + 0.5 - mean) / scale),
+        F.logsigmoid((x - 0.5 - mean) / scale))
+
+    return logp
+
+
+def log_integer_probability_standard(x):
+    logp = log_min_exp(
+        F.logsigmoid(x + 0.5),
+        F.logsigmoid(x - 0.5))
+
+
+def reparameterization(mu, log_var):
+    std = torch.exp(0.5*log_var)
+    eps = torch.randn_like(std)
+    return mu + std * eps
+
+
+########################
+
+
+
+
+
+
 class RecFusion(GeneralRecommender):
     r"""RecFusion
 
@@ -35,40 +137,20 @@ class RecFusion(GeneralRecommender):
         X = dataset.inter_matrix(form='csr').astype(np.float32)
         # just directly calculate the entire score matrix in init
         # (can't be done incrementally)
-        # print(X)
-        # print(X.shape)
-        # print(dataset)        
+        x = torch.FloatTensor(X.toarray())
+        print(x)
 
-        # pdb.set_trace()
+
+        # =====
+        # forward difussion
+        Z = [self.reparameterization_gaussian_diffusion(x, 0)]
+
+        for i in range(1, self.T):
+            Z.append(self.reparameterization_gaussian_diffusion(Z[-1], i))
         
-        # gram matrix
-        G = X.T @ X
 
-        
-        # add reg to diagonal
-        G += reg_weight * sp.identity(G.shape[0])
-
-        # convert to dense because inverse will be dense
-        G = G.todense()
-
-        # invert. this takes most of the time
-        P = np.linalg.inv(G)
-        B = P / (-np.diag(P))
-        # zero out diag
-        np.fill_diagonal(B, 0.)
-
-        # instead of computing and storing the entire score matrix,
-        # just store B and compute the scores on demand
-        # more memory efficient for a larger number of users
-        # but if there's a large number of items not much one can do:
-        # still have to compute B all at once
-        # S = X @ B
-        # self.score_matrix = torch.from_numpy(S).to(self.device)
-
-        # torch doesn't support sparse tensor slicing,
-        # so will do everything with np/scipy
-        self.item_similarity = B
-        self.interaction_matrix = X
+        # self.item_similarity = B
+        self.interaction_matrix = Z
         self.other_parameter_name = ['interaction_matrix', 'item_similarity']
 
     def forward(self):
