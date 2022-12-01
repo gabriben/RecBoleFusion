1212d296dd66743c677d34fc91bfc214b0add05c
Gabriel Bénédict <gbndict@gmail.com>
2022-12-01 13:53:36 +0100

    remove ranking metrics afterall

--- recbole/model/general_recommender/recfusion.py
+++ recbole/model/general_recommender/recfusion.py
@@ -26,7 +26,7 @@ from sklearn import datasets
 from torch.utils.data import Dataset, DataLoader
 import torch.nn as nn
 import torch.nn.functional as F
-import ranking_metrics as metric
+# import ranking_metrics as metric
 
 import pdb 
 
@@ -107,51 +107,6 @@ def reparameterization(mu, log_var):
     std = torch.exp(0.5*log_var)
     eps = torch.randn_like(std)
     return mu + std * eps
-
-
-########################
-
-import bottleneck as bn
-import numpy as np
-
-
-def NDCG_binary_at_k_batch(X_pred, heldout_batch, k=100):
-    '''
-    Normalized Discounted Cumulative Gain@k for binary relevance
-    ASSUMPTIONS: all the 0's in heldout_data indicate 0 relevance
-    '''
-    batch_users = X_pred.shape[0]
-    idx_topk_part = bn.argpartition(-X_pred, k, axis=1)
-    topk_part = X_pred[np.arange(batch_users)[:, np.newaxis],
-                       idx_topk_part[:, :k]]
-    idx_part = np.argsort(-topk_part, axis=1)
-
-    idx_topk = idx_topk_part[np.arange(batch_users)[:, np.newaxis], idx_part]
-
-    tp = 1. / np.log2(np.arange(2, k + 2))
-
-    DCG = (heldout_batch[np.arange(batch_users)[:, np.newaxis],
-                         idx_topk].toarray() * tp).sum(axis=1)
-    IDCG = np.array([(tp[:min(n, k)]).sum()
-                     for n in heldout_batch.getnnz(axis=1)])
-
-    NDCG = (DCG / IDCG)
-    return NDCG
-
-
-def Recall_at_k_batch(X_pred, heldout_batch, k=100, factor=1):
-    batch_users = X_pred.shape[0]
-
-    idx = bn.argpartition(-X_pred, k, axis=1)
-    X_pred_binary = np.zeros_like(X_pred, dtype=bool)
-    X_pred_binary[np.arange(batch_users)[:, np.newaxis], idx[:, :k]] = True
-
-    X_true_binary = (heldout_batch > 0).toarray()
-    tmp = (np.logical_and(X_true_binary, X_pred_binary).sum(axis=1)).astype(
-        np.float32)
-    recall = tmp / np.minimum(k, X_true_binary.sum(axis=1))
-
-    return recall
 
 
 ########################
