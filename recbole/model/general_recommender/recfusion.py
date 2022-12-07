d4b819e312ea3adf295f151fa9b5b9b4b72eae84
Gabriel Bénédict <gbndict@gmail.com>
2022-12-07 17:56:42 +0100

    define get_rating_matrix

--- recbole/model/general_recommender/recfusion.py
+++ recbole/model/general_recommender/recfusion.py
@@ -207,7 +207,25 @@ class RecFusion(GeneralRecommender):
 
                 # Normal Initialization for Biases
                 layer.bias.data.normal_(0.0, 0.0001)
-        
+
+    # recbole code from multvae
+    def get_rating_matrix(self, user):
+        r"""Get a batch of user's feature with the user's id and history interaction matrix.
+
+        Args:
+            user (torch.LongTensor): The input tensor that contains user's id, shape: [batch_size, ]
+
+        Returns:
+            torch.FloatTensor: The user's feature of a batch of user, shape: [batch_size, n_items]
+        """
+        # Following lines construct tensor of shape [B,n_items] using the tensor of shape [B,H]
+        col_indices = self.history_item_id[user].flatten()
+        row_indices = torch.arange(user.shape[0]).to(self.device) \
+            .repeat_interleave(self.history_item_id.shape[1], dim=0)
+        rating_matrix = torch.zeros(1).to(self.device).repeat(user.shape[0], self.n_items)
+        rating_matrix.index_put_((row_indices, col_indices), self.history_item_value[user].flatten())
+        return rating_matrix
+                
     def forward(self, x):        
         
         # x = self.interaction_matrix
