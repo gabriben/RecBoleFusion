61ca2654577ebf425f02ec25d544b8dfcb13a680
Gabriel Bénédict <gbndict@gmail.com>
2022-12-19 13:21:21 +0100

    pass for the last batch and return a loss of 1

--- recbole/model/general_recommender/recfusion.py
+++ recbole/model/general_recommender/recfusion.py
@@ -298,7 +298,7 @@ class RecFusion(GeneralRecommender):
         user = interaction[self.USER_ID]
 
         if len(user) < 200:
-            return None
+            return torch.Tensor([1])
 
         x = self.get_rating_matrix(user)[:, : -1]
 
